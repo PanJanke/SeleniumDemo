baf98f573fc7edb8fc69f56179c28254501dd9cd
Jan Krol <jan.krol.legowski@gmail.com>
2023-11-07 14:18:31 +0100

    first commit

---
+++ src/test/java/pl/jano/tests/BaseTest.java
@@ -0,0 +1,20 @@
+package pl.jano.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pl.jano.utils.DriverFactory;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+    @BeforeMethod
+    public void setup(){
+       driver = DriverFactory.getDriver();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
---
+++ src/test/java/pl/jano/tests/RegisterTest.java
@@ -0,0 +1,10 @@
+package pl.jano.tests;
+
+import org.testng.annotations.Test;
+
+public class RegisterTest {
+    @Test
+    public void registerUserTest(){
+        System.out.println("MOck");
+    }
+}
